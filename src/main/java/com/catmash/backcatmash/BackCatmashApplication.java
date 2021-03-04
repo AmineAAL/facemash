be31e796b859de66454239aadc88ac2ce584ab23
AALOUI Amine <aaaloui.ext@gmf.fr>
2021-03-04 07:21:44 +0100

    initialisation back catmash

---
+++ src/main/java/com/catmash/backcatmash/BackCatmashApplication.java
@@ -0,0 +1,13 @@
+package com.catmash.backcatmash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackCatmashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackCatmashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catmash/backcatmash/BackCatmashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catmash.backcatmash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackCatmashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
